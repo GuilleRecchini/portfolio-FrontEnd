3f99d80f7761270676a280e4ca9640a81a80993f
GuilleRecchini <guillermorecchini@gmail.com>
2022-10-07 20:03:19 -0300

    FrontEnd basado en componentes de Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { AboutComponent } from './components/about/about.component';
+import { ExperienceItemComponent } from './components/experience-item/experience-item.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationItemComponent } from './components/education-item/education-item.component';
+import { EducationComponent } from './components/education/education.component';
+import { SkillComponent } from './components/skill/skill.component';
+import { SkillItemComponent } from './components/skill-item/skill-item.component';
+import { ProyectComponent } from './components/proyect/proyect.component';
+import { ProyectItemComponent } from './components/proyect-item/proyect-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    AboutComponent,
+    ExperienceItemComponent,
+    ExperienceComponent,
+    EducationItemComponent,
+    EducationComponent,
+    SkillComponent,
+    SkillItemComponent,
+    ProyectComponent,
+    ProyectItemComponent
   ],
   imports: [
     BrowserModule,
